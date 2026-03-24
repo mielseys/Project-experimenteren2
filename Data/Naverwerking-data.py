13ca9275e6630c5aee27dc8f0770cb0ce46b7380
tristancools <tristancools2006@gmail.com>
2026-03-24 18:50:05 +0100

    fout probeeren oop te lossen, nu zijn de waarden nog steeds niet juist.

--- Data/Naverwerking-data.py
+++ Data/Naverwerking-data.py
@@ -9,7 +9,7 @@ import scipy.integrate as integrate
 # =================================================================
 
 
-sheet = pd.read_excel("Data/Kalibratie_magnetisch_veld.xlsx", 0)
+sheet = pd.read_excel("Data/Data-sheet.xlsx", 0)
 
 d = np.array(sheet['d(cm)'])
 B = np.stack((sheet['Bx (µT)'], sheet['By (µT)'], sheet['Bz (µT)']), 1) * 10**-6
@@ -35,20 +35,12 @@ B_interpolatie = interpolatie(d_interpolatie)
 # wat op een afstand van 13.69cm lag in de interpolatie van het veld.
 # We willen dus het veld in het interval [3.69, 23.69]cm hebben,
 # en dat veld dan integreren over de lengte van de fused kwarts staaf.
-# Hiervoor zullen we een mask gebruiken.
-
-mask_staaf = (3.69 < d_interpolatie) & (d_interpolatie < 23.69)
-B_staaf = B_interpolatie[mask_staaf]
-d_staaf = d_interpolatie[mask_staaf]
-
-# Gemiddeld magnetisch veld door de staaf
-
 
 def integrand(x):
     return interpolatie(x)
 
 #integratie van het veld over de lengte van de staaf
 Integraal = integrate.quad(integrand, 3.69, 23.69)
+
 print("Het veld dat door de staaf gaat is ongeveer", Integraal[0], "T*cm")
 print(Integraal[1], "is de fout op deze waarde")
-print(Integraal)
